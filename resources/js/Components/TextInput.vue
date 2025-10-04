13d102e6c8289076af93ebb576fd389c28878142
JohnArbaiza1 <johnarbaiza07.03@gmail.com>
2025-10-03 20:16:26 -0600

    feat(auth): implementando diseño del login

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -22,5 +22,6 @@ defineExpose({ focus: () => input.value.focus() });
         class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
         v-model="model"
         ref="input"
+        placeholder=""
     />
 </template>
