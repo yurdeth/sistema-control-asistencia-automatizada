a69888500dc11e5ab5bc0bad0fff6cf2ae67546c
JohnArbaiza1 <johnarbaiza07.03@gmail.com>
2025-10-08 11:44:56 -0600

    fix(Welcome.vue): botón de login funcional

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -24,6 +24,10 @@ function handleImageError() {
     document.getElementById('docs-card-content')?.classList.add('!flex-row');
     document.getElementById('background')?.classList.add('!hidden');
 }
+
+const goToLogin = () => {
+    window.location.href = '/login'
+}
 </script>
 
 <template>
@@ -65,7 +69,7 @@ function handleImageError() {
 
                         <template v-else>
                             <Link
-                                :href="route('login')"
+                                href="/login"
                                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                             >
                                 Log in
