344b9ba13a2a6b079f1b5c635e8d1c08e83ff211
Angel Vasquez <angelvasquez2570@gmail.com>
2025-11-18 22:25:54 -0600

    Configuraciones de variables de entorno para el storage

--- config/storage.php
+++ config/storage.php
@@ -2,6 +2,6 @@
 
 return [
     'url' => [
-        'img' => env('IMG_URL', config('app.url', 'http://localhost')),
+        'img' => env('IMG_URL', config('app.url', 'http://localhost') . '/storage'),
     ]
 ];
